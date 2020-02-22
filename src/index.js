4e4c5acdc5bd0659d98487fb3aa2c02fd824bf8f
Sergi Romeu <romeuhd@gmail.com>
2020-02-22 18:50:19 +0100

    add global styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import App from './pages';
+import App from './pages/App';
+import GlobalStyles from './styles/globalStyles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Fragment>
+    <GlobalStyles />
+    <App />
+  </Fragment>,
+  document.getElementById('root'),
+);
